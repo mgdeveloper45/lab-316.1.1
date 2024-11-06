50cc41492e09d91970fe00b2ad08ce5acf129cde
MarkGee45 <moel55.mg@gmail.com>
2024-11-05 18:59:06 -0800

    set background color and added h1 element

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+// Part 1: Getting Started
+
 const mainEl = document.getElementsByTagName('main');
+mainEl[0].style.backgroundColor = 'var(--main-bg)';
+
+const h1Main = document.createElement("h1");
+h1Main.textContent = `DOM Manipulation`;
+mainEl[0].appendChild(h1Main)
