5744b461a54163c1f1cdc7ca6e9777d723067e35
MarkGee45 <moel55.mg@gmail.com>
2024-11-05 19:01:46 -0800

    added classlist to center h1

--- src/index.js
+++ src/index.js
@@ -5,4 +5,6 @@ mainEl[0].style.backgroundColor = 'var(--main-bg)';
 
 const h1Main = document.createElement("h1");
 h1Main.textContent = `DOM Manipulation`;
-mainEl[0].appendChild(h1Main)
+mainEl[0].appendChild(h1Main);
+
+mainEl[0].classList.add("flex-ctr");
