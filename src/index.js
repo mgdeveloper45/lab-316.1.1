94cd6a865d035cb66151995bae6d8501322b9642
MarkGee45 <moel55.mg@gmail.com>
2024-11-05 19:41:05 -0800

    started part 2 cache nav added height and background color

--- src/index.js
+++ src/index.js
@@ -8,3 +8,9 @@ h1Main.textContent = `DOM Manipulation`;
 mainEl[0].appendChild(h1Main);
 
 mainEl[0].classList.add("flex-ctr");
+
+// Part 2: Creating a Menu Bar
+
+const topMenuEl = document.getElementById("top-menu");
+topMenuEl.style.height = "100%";
+topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
